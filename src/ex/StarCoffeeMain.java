0916110fb70a41bb1eb46ca88c963d3143a7776b
parkjihyo <wlgy5678@gmail.com>
2026-01-15 17:39:39 +0900

    수정 5

---
+++ src/ex/StarCoffeeMain.java
@@ -0,0 +1,19 @@
+package ex;
+
+public class StarCoffeeMain {
+    static void main(String[] args) {
+        // 1. 매개변수 1개 생성자 호출 (메뉴만 선택)
+        StarCoffeeMenu order1 = new StarCoffeeMenu("아메리카노");
+
+        // 2. 매개변수 2개 생성자 호출 (메뉴 + 사이즈 선택)
+        StarCoffeeMenu order2 = new StarCoffeeMenu("카페 라뗴", "Grande");
+
+        // 3. 매개변수 3개 생성자 호출 (메뉴 + 사이즈 + 핫 여부 선택)
+        StarCoffeeMenu order3 = new StarCoffeeMenu("플랫 화이트", "Venti", false);
+
+        // 각각의 주문 내역서 출력
+        order1.showMenu();
+        order2.showMenu();
+        order3.showMenu();
+    }
+}
---
+++ src/ex/StarCoffeeMenu.java
@@ -0,0 +1,37 @@
+package ex;
+
+public class StarCoffeeMenu {
+    // 멤버 변수 (상태)
+    String menuName;
+    String size;
+    boolean isHot;
+
+    // 1) 생성자: 메뉴만 정할 때 (사이즈: Tall, 핫: true 기본값)
+    public StarCoffeeMenu(String menuName) {
+        this.menuName = menuName;
+        this.size = "Tall";
+        this.isHot = true;
+    }
+
+    // 2) 생성자: 메뉴, 사이즈 정할 때 (핫: true 기본값)
+    public StarCoffeeMenu(String menuName, String size) {
+        this.menuName = menuName;
+        this.size = size;
+        this.isHot = true;
+    }
+
+    // 3) 생성자: 메뉴, 사이즈, 핫 모두 정할 때
+    public StarCoffeeMenu(String menuName, String size, boolean isHot) {
+        this.menuName = menuName;
+        this.size = size;
+        this.isHot = isHot;
+    }
+
+    // 메서드 (기능): 주문 내역서 출력
+    void showMenu() {
+        System.out.println("----주문 내역서----");
+        System.out.println("주문한 메뉴: " + menuName);
+        System.out.println("사이즈: " + size);
+        System.out.println("핫 여부: " + isHot);
+    }
+}
