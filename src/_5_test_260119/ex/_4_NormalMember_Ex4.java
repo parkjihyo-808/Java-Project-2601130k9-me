fd40629cac56f7daae04ae284f3e20d4cbc92fe2
parkjihyo <wlgy5678@gmail.com>
2026-01-19 17:38:22 +0900

    수정8

---
+++ src/_5_test_260119/ex/_4_NormalMember_Ex4.java
@@ -0,0 +1,19 @@
+package _5_test_260119.ex;
+
+public class _4_NormalMember_Ex4 extends _4_MemberBase_Ex4  {
+    public _4_NormalMember_Ex4(String name, String email, int age, String password) {
+        super(name, email, age, password); // 부모 생성자에 비번까지 전달
+    }
+
+    @Override
+    public void showInfo() {
+        public void showInfo;() {
+            System.out.println("이름: " + name + ", 이메일: " + email);
+        }
+
+        @Override
+        public void join;() {
+            System.out.println(name + "님 가입을 축하합니다!");
+        }
+    }
+}
