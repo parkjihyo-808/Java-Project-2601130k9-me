344ddbb896e4fef0869728bbd51f8524e7503a3b
parkjihyo <wlgy5678@gmail.com>
2026-01-20 16:57:03 +0900

    수정10

--- src/_6_test_260120/_1_MemberBase_Ex1.java
+++ src/_6_test_260120/_1_MemberBase_Ex1.java
@@ -17,7 +17,23 @@ public abstract class _1_MemberBase_Ex1 {
     public abstract void join();
 
     public abstract void showInfo(); // 정보 출력용 추상 메서드
+
     // 로그인 비교를 위해 필요한 것들
-    public String getEmail() {return email;}
-    public String getPassword() {return password;}
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    // 260120_실습4_풀이_업그레이드_임시저장파일_추가, 순서2-1
+    // 생성시, 우클릭 -> 생성 -> Getter -> 각각, 하나씩, name, age 선택.
+    public String getName() {
+        return name;
+    }
+
+    // 260120_실습4_풀이_업그레이드_임시저장파일_추가, 순서2-2
+    public int getAge() {
+        return age;
+    }
 }
